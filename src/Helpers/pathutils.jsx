e97d86ce1444dfe35c7768677148ad2af31b18e7
julisps <julisps@users.noreply.github.com>
2019-06-10 23:47:31 -0300

    Revert "Tnre 5947 fix tags (#36)" (#37)
    This reverts commit 2813535679c88f5e1b84f0a5e42bc092a26df48e.

--- src/Helpers/pathutils.jsx
+++
@@ -1,8 +0,0 @@
-export function cleanString(string) {
-  return string.trim()
-    .replace(/["'`,._:;|{}\[\]+=*&%^$#@!~()?<>\/\\]/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/^-+|-+$/g, '')
-    .toLowerCase();
-}
-
